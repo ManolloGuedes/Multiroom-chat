a4d7dd17942f9ce89c5969b69e8e24688a8527b2
Herlon Manollo Candido Guedes <manollo.guedes11@gmail.com>
2019-10-08 11:55:19 -0400

    [Add] - Validating nickname on application access with express-validator

--- app/controller/chat.js
+++ app/controller/chat.js
@@ -1,3 +1,15 @@
 module.exports.startChat = (application, req, res) => {
+  let formData = req.body;
+
+  req.assert('nickname', 'Name or nickname is required').notEmpty();
+  req.assert('nickname', 'Name or nickname must contain between 3 and 15 characters').len(3, 15);
+
+  const errors = req.validationErrors();
+
+  if (errors) {
+    res.render('index', { errors });
+    return;
+  }
+
   res.render('chat');
 };
